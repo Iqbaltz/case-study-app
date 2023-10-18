5ec2ae9b2826730f50f9eee083bf669b46f5e7d1
iqbaltz <mhdiqbaltan@gmail.com>
2023-10-18 11:34:21 +0700

    cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,5 @@
 import ProductList from "@/components/ProductList";
 
-// async function getProducts() {
-//   const res = await fetch("https://fakestoreapi.com/products");
-//   // The return value is *not* serialized
-//   // You can return Date, Map, Set, etc.
-
-//   if (!res.ok) {
-//     // This will activate the closest `error.js` Error Boundary
-//     throw new Error("Failed to fetch data");
-//   }
-
-//   return res.json();
-// }
-
 export default async function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
